8fcb44906f3d6cbbf10169ff34e568f15edf7eb8
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2022-01-30 11:57:39 -0500

    Update MaterNationalHook.cpp

--- MaterNationalHook.cpp
+++ MaterNationalHook.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <filesystem>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -10,58 +11,42 @@
 #include "InlineContext.h"
 #include "offsets.h"
 #include "Hooks.h"
+#include "ParameterBlockClass.h"
 
-void ParameterBlockOpenFileHook() {
-    
+constexpr auto WindowTitle = "Cars: Hi-Octane Helper Plugin";
+
+__declspec(naked) void ParameterBlockOpenFileHook() {
+
     GetInlineContext();
 
-    char* FileName = (char*)ctx.ESI; // Create variable and assign it to the ESI value retreived from GetInlineContext.
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+    }
+    // Create stack frame.
+
+    char* FileName;
+    FileName = (char*)ctx.EBP;
+    // Create variable and assign it to the EBP value retreived from GetInlineContext.
+    // This has to take 2 lines due to the way naked functions are compiled.
 
     printf("[Logging::ParameterBlock::OpenFile] Opened file: %s\n", FileName);
 
-    // SetInlineContext();
+    __asm
+    {
+        mov esp, ebp
+        pop ebp
+    }
+    // Destroy stack frame.
+
+    RETURN;
 }
 
-// Unfinished!
-void MOT_LoaderHook() {
-
-    GetInlineContext();
-
-    char* FileName = (char*)ctx.EDI; // Create variable and assign it to the EDI value retreived from GetInlineContext.
-
-    std::string FileName_STR(FileName);
-    FileName_STR += ".got";
-
-    std::ifstream MOTFile(FileName_STR, std::ios::in | std::ios::binary);
-
-    int MOT_Version = 0;
-
-    MOTFile.read((char*)&MOT_Version, 4);
-
-    MOTFile.close();
-
-    if (MOT_Version == -117440513) { // Version 8
-        printf("[Patches::MotionLibrary::LoadMotionFromMOT] Converting %s to Version -7...\n", FileName);
-        // Convert animation to version 7
-        std::string cmd = "python anim_backporter.py " + FileName_STR + " " + FileName_STR + ".backported";
-        system(cmd.c_str());
-        strcat(FileName, ".backported");
-        // Concatenate extension directly to old pointer so that LoadMotionFromMOT loads the correct file.
-        // This way, we dont need to modify the registers!
-    }
-    else if (MOT_Version == -100663297) { // Version 7
-        // Do nothing
-    }
-    else if (MOT_Version == -7) { // Version 7 MOT (Little Endian)
-        printf("[Patches::MotionLibrary::LoadMotionFromMOT] Byteswapping Animation %s...\n", FileName);
-        // Byteswap animation file
-        std::string cmd = "python byteswap_animation.py " + FileName_STR + " " + FileName_STR + ".byteswapped";
-        system(cmd.c_str());
-        strcat(FileName, ".byteswapped");
-        // Concatenate extension directly to old pointer so that LoadMotionFromMOT loads the correct file.
-        // This way, we dont need to modify the registers!
-    }
+void FileDiscovery() {
+    // Will be used for adding native mod loading support.
 }
+
 
 DWORD WINAPI main(HMODULE hModule)
 {
@@ -69,9 +54,9 @@ DWORD WINAPI main(HMODULE hModule)
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
-    SetConsoleTitleA("cars mater nation for wii u");
+    SetConsoleTitleA(WindowTitle);
 
-    InlineHook32(ParameterBlock_OpenFile + 0x16, 7, &ParameterBlockOpenFileHook); // Some simple hooks will be installed in our main thread like so.
+    InlineHook32(ParameterBlock_OpenFile + 0x34, 5, &ParameterBlockOpenFileHook); // Some simple hooks will be installed in our main thread like so.
     
     // CarsActivityUI_RequestDialogueHook::install(); // Others will be held in their own namespace, and their code will be in Hooks.h.
 
@@ -86,10 +71,10 @@ DWORD WINAPI main(HMODULE hModule)
     }
 
     // Unhook our hooks. (UNIMPLEMENTED!)
-
-	fclose(f);
-	FreeConsole();
-	FreeLibraryAndExitThread(hModule, 0);
+    
+    fclose(f);
+    FreeConsole();
+    FreeLibraryAndExitThread(hModule, 0);
     return 0;
 
 }
