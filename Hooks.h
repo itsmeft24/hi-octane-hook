562780f980d30883087a2542260af60881ba9c64
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2022-02-02 08:49:14 -0500

    rewrite dialogue list hook (not tested)

--- Hooks.h
+++ Hooks.h
@@ -1,50 +1,54 @@
 #pragma once
 
-namespace CarsActivityUI_RequestDialogueHook { // im aware that this is very overcomplicated lol, i will rewrite eventually
+namespace CarsActivityUI_RequestDialogue_Hook
+{
 
-    char* AllocatedStringTable;
-    char* AllocatedCode;
-
-    void GenerateStringTableAndHook(std::string path) { // request_dialogue_patch.bin
-        std::ifstream file(path, std::ios::in);
+    std::vector<std::string> dialogue_list_append;
+    
+    void DECL_CARSHOOK HookFunction() {
+        
+        GetInlineContext();
+        
+        __asm
+        {
+            push ebp
+            mov ebp, esp
+        }
+        
+        {
+            for (int x = 0; x < dialogue_list_append.size()) {
+                CarsActivity_AddNameToDialogueList(ctx.ECX, dialogue_list_append[x].c_str());
+            }
+        }
+        
+        __asm
+        {
+            mov esp, ebp
+            pop ebp
+            ret
+        }
+        
+    }
+    
+    void CollectCharactersToPatch() {
+        std::ifstream file("DataPC\\C\\Global\\Chars\\DialogueList.ini", std::ios::in);
         std::string line;
         std::vector<std::string> lines;
-        std::vector<int> table_offsets;
         int size = 0;
         while (std::getline(file, line)) {
-            table_offsets.push_back(size); // is really the offset in the allocated string table
             lines.push_back(line);
             size += line.size() + 1;
         }
         file.close();
-        AllocatedStringTable = new char[size]; // if this doesnt work then use VirtualAlloc
-        int code_pos = 0;
-        AllocatedCode = AllocateCode(12 * lines.size());
-        for (int x = 0; x < lines.size(); x++) {
-            memcpy(AllocatedStringTable + table_offsets[x], lines[x].c_str(), lines[x].size());
-
-            DWORD relativeAddress = (DWORD)(AllocatedStringTable + table_offsets[x] - (DWORD)AllocatedCode + code_pos) - 5;
-
-            *(AllocatedCode + code_pos) = 0x68;
-            *(DWORD*)(AllocatedCode + code_pos + 1) = relativeAddress;
-            // push OFFSET string
-            *(WORD*)(AllocatedCode + code_pos + 5) = 0xF189;
-            // mov ecx, esi
-            WriteCALL(AllocatedCode + code_pos + 7, CarsActivity_AddNameToDialogueList);
-            // call CarsActivity_AddNameToDialogueList
-            code_pos += 12;
-        }
     }
-
+    
     void install() {
-        GenerateStringTableAndHook("request_dialogue_patch.bin");
-        InlineHook32(CarsActivityUI_RequestDialogue + 0x105, 5, AllocatedCode);
+        CollectCharactersToPatch();
+        InlineHook32(CarsActivityUI_RequestDialogue + 0x105, 5, &HookFunction);
     }
-
+    
     void uninstall() {
-        RemoveInlineHook32(CarsActivityUI_RequestDialogue + 0x105, 5, AllocatedCode);
-        FreeCode(AllocatedCode);
-        delete[] AllocatedStringTable;
+        RemoveInlineHook32(CarsActivityUI_RequestDialogue + 0x105, 5, &HookFunction);
     }
-
+    
 };
