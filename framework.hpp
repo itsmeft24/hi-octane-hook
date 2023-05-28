3ede09b6efa20c1b1c7e8b274f0bdf293f435a48
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2023-05-28 19:10:07 -0400

    push new hooking framework

---
+++ framework.hpp
@@ -0,0 +1,217 @@
+#pragma once
+
+#include "relocate_code.hpp"
+#include <cstdint>
+#include <format>
+#include <detours/detours.h>
+
+#define DefineReplacementHook(name) \
+struct name : public hooking::impl::ReplacementHook<name>
+
+#define DefineInlineHook(name) \
+struct name : public hooking::impl::InlineHook<name>
+
+template <class T> constexpr bool is_ptr = std::is_integral_v<T> || std::is_pointer_v<T>;
+
+namespace winapi {
+    enum class Perm : DWORD {
+        None = PAGE_NOACCESS,
+        Read = PAGE_READONLY,
+        ReadWrite = PAGE_READWRITE,
+        WriteCopy = PAGE_WRITECOPY,
+        Execute = PAGE_EXECUTE,
+        ExecuteRead = PAGE_EXECUTE_READ,
+        ExecuteReadWrite = PAGE_EXECUTE_READWRITE,
+        ExecuteWriteCopy = PAGE_EXECUTE_WRITECOPY,
+        Guard = PAGE_GUARD,
+        NoCache = PAGE_NOCACHE,
+        WriteCombine = PAGE_WRITECOMBINE,
+    };
+    // Sets the desired permission on the memory block.
+    template <typename T, typename std::enable_if_t<is_ptr<T>>* = nullptr>
+    inline std::pair<Perm, bool> set_permission(T ptr, size_t size, Perm perm) {
+        Perm old_perm = Perm::None;
+        bool success = static_cast<bool>(VirtualProtect(reinterpret_cast<void*>(ptr), size, static_cast<DWORD>(perm), reinterpret_cast<DWORD*>(&old_perm)));
+        return std::make_pair(old_perm, success);
+    }
+
+    template <typename T> inline T* allocate_code(size_t count) {
+        return reinterpret_cast<T*>(VirtualAlloc(NULL, sizeof(T) * count, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
+    }
+};
+
+namespace hooking {
+
+    template <typename T1, typename std::enable_if_t<is_ptr<T1>>* = nullptr, typename T2, typename std::enable_if_t<is_ptr<T2>>* = nullptr>
+    inline void write_jmp(T1 src, T2 dst) {
+        uintptr_t relativeAddress = (uintptr_t)((std::uint8_t*)dst - (uintptr_t)src) - 5;
+
+        winapi::set_permission(src, 5, winapi::Perm::ExecuteReadWrite);
+        *(std::uint8_t*)src = 0xE9;
+        *(uintptr_t*)((uintptr_t)src + 1) = relativeAddress;
+    }
+
+    template <typename T1, typename std::enable_if_t<is_ptr<T1>>* = nullptr, typename T2, typename std::enable_if_t<is_ptr<T2>>* = nullptr>
+    inline void write_call(T1 src, T2 dst) {
+        uintptr_t relativeAddress = (uintptr_t)((std::uint8_t*)dst - (uintptr_t)src) - 5;
+
+        winapi::set_permission(src, 5, winapi::Perm::ExecuteReadWrite);
+        *(std::uint8_t*)src = 0xE8;
+        *(uintptr_t*)((uintptr_t)src + 1) = relativeAddress;
+    }
+
+    template <typename T1, typename std::enable_if_t<is_ptr<T1>>* = nullptr, typename T2, typename std::enable_if_t<is_ptr<T2>>* = nullptr>
+    inline void write_push(T1 src, T2 dst) {
+        winapi::set_permission(src, 5, winapi::Perm::ExecuteReadWrite);
+        *(std::uint8_t*)src = 0x68;
+        *(uintptr_t*)((uintptr_t)src + 1) = (uintptr_t)dst;
+    }
+
+    template <typename T, typename std::enable_if_t<is_ptr<T>>* = nullptr>
+    inline bool write_nop(T addr, size_t code_size) {
+        const auto& [original_protection, success] = winapi::set_permission(addr, code_size, winapi::Perm::ExecuteReadWrite);
+        if (success) {
+            std::memset(reinterpret_cast<void*>(addr), 0x90, code_size);
+            return true;
+        }
+        return false;
+    }
+
+    union Register {
+        void* pointer;
+        std::uint32_t unsigned_integer;
+        std::int32_t signed_integer;
+        float floating_point;
+    };
+
+    struct InlineContext {
+		Register eflags;
+		Register edi;
+		Register esi;
+		Register ebp;
+		Register esp;
+		Register ebx;
+		Register edx;
+		Register ecx;
+		Register eax;
+
+        inline void print() {
+            std::cout << std::format("eax: {:#X}\necx: {:#X}\nedx: {:#X}\nebx: {:#X}\nesp: {:#X}\nebp: {:#X}\nesi: {:#X}\nedi: {:#X}\neflags: {:#X}\n",
+                eax.unsigned_integer,
+                ecx.unsigned_integer,
+                edx.unsigned_integer,
+                ebx.unsigned_integer,
+                esp.unsigned_integer,
+                ebp.unsigned_integer,
+                esi.unsigned_integer,
+                edi.unsigned_integer,
+                eflags.unsigned_integer
+            ) << std::endl;
+        }
+    };
+
+    static_assert(sizeof(InlineContext) == 36);
+
+    template<typename R, typename... A>
+    using GenericFuncPtr = R(*)(A...);
+
+    namespace impl {
+
+        template<typename Derived>
+        class ReplacementHook {
+
+            template<typename T = Derived>
+            using CallbackFuncPtr = decltype(&T::callback);
+
+            static inline auto& orig_ref() {
+                static constinit CallbackFuncPtr<Derived> s_func_ptr = nullptr;
+
+                return s_func_ptr;
+            }
+
+        public:
+            template<typename... Args>
+            static inline decltype(auto) original(Args &&... args) {
+                return orig_ref()(std::forward<Args>(args)...);
+            }
+
+            template<typename R, typename ...A>
+            static inline void install_at_func_ptr(GenericFuncPtr<R, A...> ptr) {
+                using ArgFuncPtr = decltype(ptr);
+
+                static_assert(std::is_same_v<ArgFuncPtr, CallbackFuncPtr<>>, "Argument pointer type must match callback type!");
+
+                orig_ref() = ptr;
+
+                DetourTransactionBegin();
+                DetourUpdateThread(GetCurrentThread());
+                DetourAttach(reinterpret_cast<void**>(&orig_ref()), Derived::callback);
+                DetourTransactionCommit();
+            }
+
+            static inline void install_at_ptr(uintptr_t ptr) {
+
+                orig_ref() = CallbackFuncPtr<>(ptr);
+
+                DetourTransactionBegin();
+                DetourUpdateThread(GetCurrentThread());
+                DetourAttach(reinterpret_cast<void**>(&orig_ref()), Derived::callback);
+                DetourTransactionCommit();
+            }
+
+            static inline void uninstall() {
+                DetourTransactionBegin();
+                DetourUpdateThread(GetCurrentThread());
+                DetourDetach(reinterpret_cast<void**>(&orig_ref()), Derived::callback);
+                DetourTransactionCommit();
+            }
+        };
+
+        template<typename Derived>
+        class InlineHook {
+
+            template<typename T = Derived>
+            using CallbackFuncPtr = decltype(&T::callback);
+
+        public:
+
+            static inline void install_at_ptr(uintptr_t ptr) {
+                static_assert(std::is_same_v<void(__cdecl*)(InlineContext), CallbackFuncPtr<>>, "Callback function must be void and take an InlineContext!");
+
+                // Calculate the minimum bytes needed to be backed up, and an upper-bound limit of how many bytes the relocated code could take. (Used for below allocation)
+                auto [original_code_len, padded_code_len] = find_suitable_backup_size(ptr);
+                
+                if (original_code_len < 5) throw std::exception();
+				
+                // Allocate code for inline handler.
+                std::uint8_t* jit_area = winapi::allocate_code<std::uint8_t>(9 + padded_code_len + 5);
+                
+                // Build inline handler.
+                *jit_area = 0x60;
+                jit_area++; // pushad
+                *jit_area = 0x9C;
+                jit_area++; // pushfd
+                write_call(jit_area, Derived::callback);
+                jit_area += 5; // call Derived::callback
+                *jit_area = 0x9D;
+                jit_area++; // popfd
+                *jit_area = 0x61;
+                jit_area++; // popad
+                
+                // Attempt to build/relocate the code, and if successful, copy into the trampoline.
+                auto relocated = relocate_code(ptr, original_code_len, reinterpret_cast<uintptr_t>(jit_area));
+                if (relocated.has_value()) {
+                    std::memcpy(jit_area, relocated.value().data(), relocated.value().size());
+                    jit_area += relocated.value().size();
+                    write_jmp(jit_area, ptr + 5); jit_area += 5; // jmp ptr
+                    // Insert jmp from the source to the inline handler.
+                    const auto& [old_perm, success] = winapi::set_permission(ptr, original_code_len, winapi::Perm::ExecuteReadWrite);
+                    write_nop(ptr, original_code_len);
+                    // Ensure original function has the trampoline area nop'd out.
+                    write_jmp(ptr, jit_area - (9 + relocated.value().size() + 5));
+                    winapi::set_permission(ptr, original_code_len, old_perm);
+                }
+            }
+        };
+    };
+};
--- patches/WidescreenPatch.cpp
+++ patches/WidescreenPatch.cpp
@@ -4,50 +4,57 @@
 #include <fstream>
 
 #include "../ConfigManager.h"
-#include "../HookFunction.h"
+//#include "../HookFunction.h"
 #include "../Logging.h"
 #include "../Utils.h"
 #include "../FileSystem.h"
 
 #include "WidescreenPatch.h"
+#include "../framework.hpp"
 
 
 DWORD *pWindowWidth = (DWORD *)0x006FAA54;
 DWORD *pWindowHeight = (DWORD *)0x006FAA58;
 DWORD *pScreenMode = (DWORD *)0x006F72CC;
 
-DeclareFunction(BOOL, __stdcall, GetDimensionsFromSelectedResolution, 0x00414ce0, int, unsigned int*, unsigned int*);
+//__declspec(naked) void CMPPatch() { // Force the game to compare against an
+//                                    // intermediate 1, rather than the contents
+//                                    // of register EDI.
+//  __asm {
+//		cmp [pScreenMode], 1
+//		ret
+//  }
+//}
 
-__declspec(naked) void CMPPatch() { // Force the game to compare against an
-                                    // intermediate 1, rather than the contents
-                                    // of register EDI.
-  __asm {
-		cmp [pScreenMode], 1
-		ret
-  }
-}
+DefineInlineHook(CMPPatch) {
+    static void __cdecl callback(hooking::InlineContext ctx) {
+        ctx.edi.unsigned_integer = 1;
+    }
+};
 
-BOOL __stdcall GetDimensionsFromSelectedResolutionHook(WideScreenPatch::SDResolution selected, unsigned int* width, unsigned int* height) {
-    if (ConfigManager::IsWidescreenEnabled) {
-        const auto& [selected_width, selected_height] =  resolve_hd(selected);
-        *width = selected_width;
-        *height = selected_height;
+DefineReplacementHook(SelectDimensionsFromSaveIndex) {
+    static BOOL __stdcall callback(WideScreenPatch::SDResolution selected, unsigned int* width, unsigned int* height) {
+        if (ConfigManager::IsWidescreenEnabled) {
+            const auto& [selected_width, selected_height] = resolve_hd(selected);
+            *width = selected_width;
+            *height = selected_height;
+        }
+        else {
+            const auto& [selected_width, selected_height] = resolve_sd(selected);
+            *width = selected_width;
+            *height = selected_height;
+        }
+        return TRUE;
     }
-    else {
-        const auto& [selected_width, selected_height] = resolve_sd(selected);
-        *width = selected_width;
-        *height = selected_height;
-    }
-    return TRUE;
-}
+};
 
 void WideScreenPatch::install() {
   if (ConfigManager::IsWidescreenEnabled) {
-    Logging::log(
-        "[WideScreenPatches::Install] Setting screen mode to widescreen...");
+    Logging::log("[WideScreenPatches::Install] Setting screen mode to widescreen...");
 
-    HookFunction(0x00421E22, &CMPPatch, 6, FunctionHookType::InlineReplacement);
-    HookFunction((void*&)GetDimensionsFromSelectedResolution, &GetDimensionsFromSelectedResolutionHook, 7, FunctionHookType::EntireReplacement);
+
+    SelectDimensionsFromSaveIndex::install_at_ptr(0x00414ce0);
+    CMPPatch::install_at_ptr(0x00421e22);
 
     std::filesystem::path save = FileSystem::save_dir();
     save.concat("GlobalData");
@@ -55,11 +62,15 @@ void WideScreenPatch::install() {
     save_file.seekg(0x18);
     std::uint8_t i = 0;
     save_file.read((char*)&i, 1);
-    GetDimensionsFromSelectedResolutionHook((SDResolution)i, (unsigned int*)pWindowWidth, (unsigned int*)pWindowHeight);
     save_file.close();
 
+    const auto& [selected_width, selected_height] = resolve_hd((SDResolution)i);
+    *pWindowWidth = selected_width;
+    *pWindowHeight = selected_height;
+
     *pScreenMode = 2;
-    SetExecuteReadWritePermission((DWORD *)(0x00421DEF), 4);
-    *(DWORD *)(0x00421DEF) = 2; // (mov edi, 2) instead of (mov edi, 1)
+    
+    winapi::set_permission(0x00421DEF, 4, winapi::Perm::ExecuteReadWrite);
+    *reinterpret_cast<std::uint32_t*>(0x00421DEF) = 2; // (mov edi, 2) instead of (mov edi, 1)
   }
 }
